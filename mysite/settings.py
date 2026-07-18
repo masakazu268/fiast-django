c092d6564e3c57dff496e3c86c909f50c2876a56
masakazu268 <dadofakane@gmail.com>
2026-07-18 19:55:58 +0900

    fiast django3

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,11 +26,8 @@ SECRET_KEY = 'django-insecure-4nhzvh!$q#$#9i-sqf7tv0s%lqn2k1csq&rwfg9_pthhaee@=v
 DEBUG = True
 
 
-ALLOWED_HOSTS = [
-    'fiast-django.onrender.com',  # RenderのURL
-    '127.0.0.1',                  # パソコンで開発する用
-    'localhost',                  # パソコンで開発する用
-]
+ALLOWED_HOSTS = ['*']
+    
 
 
 
